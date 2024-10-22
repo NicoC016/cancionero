e12f56d8fa5aec71e8d6f39358ce8b8e5e73fc69
NicoC016 <nicoconil2018@gmail.com>
2024-10-22 19:25:53 -0300

    Cambio url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import ShowListSong from "./songs/showListSong/showListSong";
-import { orderSongs } from "./utils/song.helper";
+
 
 export default async function Home() {
-  const res = await fetch(`https://cancionero-beryl.vercel.app/api/songs`);
-  const songs = await res.json();
-  const songsView = orderSongs(songs);
-  return (
-    <div>
-      <h1 className="text-lg text-center mb-5 pt-3">Canciones:</h1>
-       <ShowListSong songsfind= {songsView}></ShowListSong>
-    </div>
-  );
+  // const res = await fetch(`https://cancionero-beryl.vercel.app/api/songs`);
+  // const songs = await res.json();
+  // const songsView = orderSongs(songs);
+  // return (
+  //   <div>
+  //     <h1 className="text-lg text-center mb-5 pt-3">Canciones:</h1>
+  //      <ShowListSong songsfind= {songsView}></ShowListSong>
+  //   </div>
+  // );
 }
